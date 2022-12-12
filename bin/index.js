f08ef66e701cfd21752289afcc63224723212e27
Sam Adams <sam.adams7785@gmail.com>
2022-12-12 14:58:49 -0800

    Prompt and sql additions

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-const prompt = require('prompt-sync')();
-const fetch = require('cross-fetch');
+const chalk = require("chalk");
+const prompt = require("prompt-sync")();
+const fetch = require("cross-fetch");
 
-const { fetchRoom } = require('../fetch-utils');
+const { fetchRoom } = require("../fetch-utils");
 
-const {
-  logCabinHC,
-} = require('../ascii');
-require('dotenv').config();
+const { logCabinHC } = require("../ascii");
+require("dotenv").config();
 
 // prompt to enter name
 // call API to load room 1
 // prompt for user action
 // 'investigate table' --> return table data w/objects
-
 
 //          console commands
 // console.log(chalk.bold.green('Welcome to the game!'));
@@ -24,20 +21,32 @@ require('dotenv').config();
 //   prompt(chalk.bgGray.green('Press any key to start your escape!'));
 //   console.log(chalk.bold.bgBlueBright.magenta('You find yourself in a lonely cabin in a lonely wood'));
 
-
 async function loadPrompts() {
-  const name = prompt(chalk.italic.bgWhite.blue('What is your name?'));
-  console.log(chalk.bold.bgYellowBright.green(`Hello, ${name}. Are you ready to begin?`));
-  prompt(chalk.bgGray.green('Press any key to continue'));
-  
+  const name = prompt(chalk.italic.bgWhite.blue("What is your name? "));
+  console.log(
+    chalk.bold.bgYellowBright.green(`Hello, ${name}. Are you ready to begin?`)
+  );
+  prompt(chalk.bgGray.green("Press any key to continue"));
+
   // opening image of cabin, needs description of user predicament
   // console.log(logCabinHC);
-
 
   // call API, gets room description, prompts for user action
   //      later --> display room options (room1, room2, etc.)
   // need fetch-utils
   const room = await fetchRoom();
-  console.log(room);
+  console.log(room[0].room_description);
+  console.log("The Objects in the room are...");
+  console.log("1. Desk");
+  console.log("2. Bunk Beds");
+  console.log("3. A Small metal Lock Box");
+  console.log("4. Window");
+  console.log("5. Door");
+  const object = prompt("Which object would you like to investigate? ");
+
+  console.log(room[0]);
+
+  console.log(object);
+  // const temp = await fetchObjects(object);
 }
 loadPrompts();
