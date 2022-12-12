939f830f8e0a56400a93f1f4247222989a19ca84
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-12 12:34:56 -0800

    add fetch utils and connect to backend repo

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,43 @@
 #!/usr/bin/env node
 const chalk = require('chalk');
 const prompt = require('prompt-sync')();
+const fetch = require('cross-fetch');
+
+const { fetchRoom } = require('../fetch-utils');
 
 const {
-  skyrimCart,
-  cabin,
-  skyrim200,
-  cabin200,
-  terminalWoodsColossal,
-  terminalForestBolger,
-  terminalForestCosmike,
-  terminalForestKban,
-  terminalGrove08,
-  terminalGroveThick,
-  cabinSm,
-  logCabin,
   logCabinHC,
-  logCabinEx,
 } = require('../ascii');
 require('dotenv').config();
 
+// prompt to enter name
+// call API to load room 1
+// prompt for user action
+// 'investigate table' --> return table data w/objects
+
+
+//          console commands
+// console.log(chalk.bold.green('Welcome to the game!'));
+// console.log(terminalForestBolger);
+//   prompt(chalk.bgGray.green('Press any key to continue'));
+//   console.log(chalk.bold.bgYellowBright.magentaBright('It\'s time to make your escape'));
+//   prompt(chalk.bgGray.green('Press any key to start your escape!'));
+//   console.log(chalk.bold.bgBlueBright.magenta('You find yourself in a lonely cabin in a lonely wood'));
+
+
 async function loadPrompts() {
-  //   console.log(chalk.bold.green('Welcome to the game!'));
-  console.log(terminalForestBolger);
+  const name = prompt(chalk.italic.bgWhite.blue('What is your name?'));
+  console.log(chalk.bold.bgYellowBright.green(`Hello, ${name}. Are you ready to begin?`));
   prompt(chalk.bgGray.green('Press any key to continue'));
-  console.log(logCabinHC);
-  //   const name = prompt(chalk.italic.bgWhite.blue('What is your name?'));
-  //   console.log(chalk.bold.bgYellowBright.green(`Hello, ${name}. Are you ready to begin?`));
-  //   prompt(chalk.bgGray.green('Press any key to continue'));
-  //   console.log(chalk.bold.bgYellowBright.magentaBright('It\'s time to make your escape'));
-  //   prompt(chalk.bgGray.green('Press any key to start your escape!'));
-  //   console.log(chalk.bold.bgBlueBright.magenta('You find yourself in a lonely cabin in a lonely wood'));
+  
+  // opening image of cabin, needs description of user predicament
+  // console.log(logCabinHC);
+
+
+  // call API, gets room description, prompts for user action
+  //      later --> display room options (room1, room2, etc.)
+  // need fetch-utils
+  const room = await fetchRoom();
+  console.log(room);
 }
 loadPrompts();
---
+++ fetch-utils.js
@@ -0,0 +1,16 @@
+const fetch = require('cross-fetch');
+
+async function fetchRoom() {
+  const response = await fetch(`${process.env.API_URL}/api/v1/roomOne`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+module.exports = { fetchRoom };
